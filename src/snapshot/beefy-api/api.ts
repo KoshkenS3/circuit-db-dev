158ffb8c5d6f8e65ed649519f89a1140405ecb6c
koshkens <cawa.kakaka@mail.ru>
2024-02-10 17:35:58 +0400

    change api to circuit

--- src/snapshot/beefy-api/api.ts
+++ src/snapshot/beefy-api/api.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import type { ApyResponse, LpBreakdownResponse, PriceResponse, TvlResponse } from './types.js';
 
 async function get<T>(path: string, cacheBuster: string): Promise<T> {
-  const response = await fetch(`https://api.beefy.finance/${path}?_=${cacheBuster}`);
+  const response = await fetch(`https://api.circuit.farm/${path}?_=${cacheBuster}`);
   return response.json() as T;
 }
 
