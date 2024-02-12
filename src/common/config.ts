074db65f2019631de631bfc5b2129814d382b6b1
koshkens <cawa.kakaka@mail.ru>
2024-02-12 19:00:03 +0400

    add circut.farm in cors

--- src/common/config.ts
+++ src/common/config.ts
@@ -17,10 +17,11 @@ export const DATABASE_SSL: string | undefined = process.env['DATABASE_SSL'];
 export const DATABASE_MIN_POOL_SIZE: number = getNumberEnv('DATABASE_MIN_POOL_SIZE', 10);
 export const DATABASE_MAX_POOL_SIZE: number = getNumberEnv('DATABASE_MAX_POOL_SIZE', 50);
 export const API_PORT: number = getNumberEnv('PORT', 4000);
-export const API_CORS_ORIGIN: RegExp = new RegExp(
-  process.env['API_CORS_ORIGIN'] ||
-    '^(https:\\/\\/app\\.beefy\\.(com|finance)|http:\\/\\/localhost(:[0-9]+)?|http:\\/\\/127.0.0.1(:[0-9]+)?)$'
-);
+// export const API_CORS_ORIGIN: RegExp = new RegExp(
+//   process.env['API_CORS_ORIGIN'] ||
+//     '^(https:\\/\\/app\\.beefy\\.(com|finance)|http:\\/\\/localhost(:[0-9]+)?|http:\\/\\/127.0.0.1(:[0-9]+)?)$'
+// );
+export const API_CORS_ORIGIN = ['circut.farm', 'www.circut.farm', 'localhost'];
 export const API_RANGE_KEY: string | undefined = process.env['API_RANGE_KEY'];
 export const SNAPSHOT_INTERVAL: number = getNumberEnv('SNAPSHOT_INTERVAL', 15 * 60);
 export const SNAPSHOT_RETRY_DELAY: number = getNumberEnv('SNAPSHOT_RETRY_DELAY', 60);
